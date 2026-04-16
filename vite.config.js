c4a10c8bea1fbdfec6a414516bfcc66debdc09ef
Loqmanmk <makouriloqman@gmail.com>
2026-04-16 01:46:13 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        rolldownOptions: {
+            output: {
+                manualChunks: {
+                    'react-vendor': ['react', 'react-dom'],
+                    'firebase-vendor': ['firebase/app', 'firebase/firestore', 'firebase/analytics'],
+                    'emailjs-vendor': ['@emailjs/browser'],
+                }
+            }
+        },
+        chunkSizeWarningLimit: 600,
+    }
 })
